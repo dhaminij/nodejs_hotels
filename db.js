bff70f5eabbcec579c42235974ae63b7334c3019
dhaminisj <dhaminijyothi@gmail.com>
2024-12-18 12:16:40 +0530

    added db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const mongoURL = 'mongodb://localhost:27017/hotels';
-
+// const mongoURL = 'mongodb://localhost:27017/hotels';
+const mongoURL = 'mongodb+srv://helloworld:dhamini123@cluster0.yprw1.mongodb.net/'
 mongoose.connect(mongoURL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
